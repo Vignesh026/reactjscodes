d7dd23ebcf729be87c234553be815ee870f13d3f
Vignesh026 <47323937+Vignesh026@users.noreply.github.com>
2020-08-21 13:33:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class One extends Component {
+  constructor() {
+    super();
+    this.state = Date();
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state}</h1>
+      </div>
+    );
+  }
 }
+export default One;
+
